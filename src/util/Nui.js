2744c393ee5caae86d095b5d44dbb748d611490a
2277 <38501234+2277@users.noreply.github.com>
2019-08-09 10:55:04 +0100

    Update Nui.js

--- src/util/Nui.js
+++ src/util/Nui.js
@@ -1,12 +1,11 @@
 export default {
   async send(event, data = {}) {
-      
     /// #if DEBUG
-    return await new Promise(resolve => setTimeout(resolve, 100));
+    return new Promise(resolve => setTimeout(resolve, 100));
     /// #endif
 
     /* eslint-disable no-unreachable */
-    return await fetch(`http://<resource>/${event}`, {
+    return fetch(`http://<resource>/${event}`, {
       method: 'post',
       headers: {
         'Content-type': 'application/json; charset=UTF-8',
